6dbfe7fc64bc4822fc23b33aa52c4781bbd17ec1
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-12-18 23:57:32 +0800

    added FireConn internal class

--- addons.py
+++ addons.py
@@ -46,3 +46,26 @@ class AddonsManager:
             json.dump(AddonsManager.config, open('config.txt', 'w'))
         except Exception as e:
             return "ERROR: Failed to delete config key; error: {}".format(e)
+        
+class FireConn:
+    @staticmethod
+    def checkPermissions():
+        if 'FireRTDBEnabled' in os.environ and os.environ['FireRTDBEnabled'] == 'True':
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def connect():
+        if not os.path.exists("serviceAccountKey.json"):
+            return "ERROR: Failed to connect to Firebase. The file serviceAccountKey.json was not found. Please re-read instructions for the Firebase addon."
+        else:
+            if 'RTDB_URL' not in os.environ:
+                return "ERROR: Failed to connect to Firebase. RTDB_URL environment variable not set in .env file. Please re-read instructions for the Firebase addon."
+            try:
+                ## Firebase
+                cred_obj = firebase_admin.credentials.Certificate(os.path.join(os.getcwd(), "serviceAccountKey.json"))
+                default_app = firebase_admin.initialize_app(cred_obj, { 'databaseURL': os.environ["RTDB_URL"] })
+            except Exception as e:
+                return "ERROR: Error occurred in connecting to RTDB; error: {}".format(e)
+            return True
