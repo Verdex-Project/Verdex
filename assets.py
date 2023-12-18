80439b2af3a6f3d99ba8c39ce445d99bbe629c67
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-12-18 11:30:37 +0800

    added a bunch of logos and made assets service
    request arguments to logo endpoint on assets domain of requests affect logo file returned

--- assets.py
+++ assets.py
@@ -1,2 +1,18 @@
-from flask import Flask, Blueprint
+from main import fileContent
+from flask import Flask, Blueprint, request, send_file, send_from_directory
 
+assetsBP = Blueprint('assets', __name__)
+
+@assetsBP.route("/assets/logo", methods=["GET"])
+def logo():
+    type = request.args.get("type")
+    if type == "transparentBlack":
+        return send_file("assets/logos/transparentLogoBlack.png", mimetype="image/png")
+    elif type == "transparentColour":
+        return send_file("assets/logos/transparentLogoColour.png", mimetype="image/png")
+    elif type == "transparentWhite":
+        return send_file("assets/logos/transparentLogoWhite.png", mimetype="image/png")
+    elif type == "icon":
+        return send_file("assets/logos/icon.png", mimetype="image/png")
+    else:
+        return send_file("assets/logos/logoColour.png", mimetype="image/png")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json, random, time, sys, subprocess, os, shutil, copy, requests, datetime
 from flask import Flask, request, render_template, redirect, url_for, flash, Blueprint
 from flask_cors import CORS
+from models import *
 
 app = Flask(__name__)
 CORS(app)
@@ -10,4 +11,10 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    # Register routes
+    
+    ## Assets service
+    from assets import assetsBP
+    app.register_blueprint(assetsBP)
+
     app.run(port=8000, host='0.0.0.0', debug=True)
