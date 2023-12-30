d89c5b49dde250581b4d4173ae5642a88d2c3c4e
Lincoln Lim <lincolnlim267@gmail.com>
2023-12-30 10:59:20 +0800

    updated DI class in models.py

--- models.py
+++ models.py
@@ -13,13 +13,19 @@ def fileContent(filePath, passAPIKey=False):
 class DI:
     data = []
 
+    sampleData = {
+        "accounts": {},
+        "itineraries": {},
+        "forum": {}
+    }
+
     @staticmethod
     def setup():
         if not os.path.exists(os.path.join(os.getcwd(), "database.txt")):
             with open("database.txt", "w") as f:
-                f.write("{}")
+                json.dump(DI.sampleData, f)
         
-        if FireConn.checkPermissions():
+        if FireRTDB.checkPermissions():
             print("DI: Firebase RTDB is enabled. Connecting to Firebase...")
             try:
                 response = FireConn.connect()
@@ -39,9 +45,9 @@ class DI:
         try:
             if not os.path.exists(os.path.join(os.getcwd(), "database.txt")):
                 with open("database.txt", "w") as f:
-                    f.write("{}")
+                    json.dump(DI.sampleData, f)
             
-            if FireConn.checkPermissions():
+            if FireRTDB.checkPermissions():
                 # Fetch data from RTDB
                 fetchedData = FireRTDB.getRef()
                 if isinstance(fetchedData, str) and fetchedData.startswith("ERROR"):
@@ -89,7 +95,7 @@ class DI:
                 json.dump(DI.data, f)
 
             # Update RTDB
-            if FireConn.checkPermissions():
+            if FireRTDB.checkPermissions():
                 response = FireRTDB.setRef(FireRTDB.translateForCloud(DI.data))
                 if response != True:
                     print("DI FIRERTDB SETREF ERROR: " + response)
