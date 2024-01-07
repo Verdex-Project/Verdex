050bacf4e2be2a9c1b37fbc64f8aad471e6703de
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-01-07 17:56:06 +0800

    Switched to using DI and create post feature works

--- templates/forum/forum.py
+++ templates/forum/forum.py
@@ -9,25 +9,11 @@ forumBP = Blueprint("forum", __name__)
 
 @forumBP.route('/', methods=['GET', 'POST'])
 def verdextalks():
-    global postsInfoJson
-
-    postsInfoJson = postsInfoJson if 'existing_data' in globals() else {}
-
-    if request.method == 'GET':
-        with open('templates/forum/postsInfo.json', 'r') as file:
-            postsInfoJson = json.load(file)
-
     if request.method == 'POST' and 'addNewPost' in request.form:
-        if not (request.form.get('post_title') and request.form.get('post_description') and request.form.get('user_names')):
-            with open('templates/forum/postsInfo.json', 'w') as file:
-                json.dump(postsInfoJson, file, indent=4)
-        else:
+        if (request.form.get('post_title') and request.form.get('post_description') and request.form.get('user_names')):
             post_title = request.form.get('post_title')
             post_description = request.form.get('post_description')
             user_names = request.form.get('user_names')
-            # Load existing data
-            with open('templates/forum/postsInfo.json', 'r') as json_file:
-                postsInfoJson = json.load(json_file)
 
             new_post = {
                 "user_names": user_names,
@@ -36,17 +22,16 @@ def verdextalks():
                 "likes": 0
             }
 
-            post_id = len(postsInfoJson) + 1
-            while str(post_id) in postsInfoJson:
-                day_number += 1
-            postsInfoJson[str(post_id)] = new_post
+            postDateTime = datetime.datetime.now().strftime(Universal.systemWideStringDatetimeFormat)
+            DI.data["forum"][postDateTime] = new_post
 
-            with open('templates/forum/postsInfo.json', 'w') as file:
-                json.dump(postsInfoJson, file, indent=4)
-                
+            #JUST FOR TESTING, REMOVE ASAP.
+            print("-----DI-----") 
+            print(DI.data)
+            print("-----END-----")
+            # REMOVE ASAP.
+
+            DI.save() 
             return redirect(url_for('forum.verdextalks'))
 
-    return render_template("forum/forum.html", postsInfoJson=postsInfoJson)
-
-# postsInfoJson=jsonify(DI.data["forum"])
-
+    return render_template("forum/forum.html", postsInfoJson=DI.data["forum"])
