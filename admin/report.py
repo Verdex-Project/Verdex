5822a0eed6970321b7ee63196463250995d03384
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-01-07 23:16:48 +0800

    pivoted log messages to be logged via Logger

--- admin/report.py
+++ admin/report.py
@@ -3,7 +3,6 @@ import os
 from main import Logger
 from flask import Blueprint, render_template, json, request, send_file, flash, redirect, url_for
 
-
 reportBP = Blueprint("report", __name__)
 
 #Main report displaying webpage
@@ -19,43 +18,25 @@ def download_report(report_id):
     report_id = request.form['report_id']
 
     #Checks if reportsInfo.json file exists, added this after merging
-    if not os.path.isfile(os.path.join(os.getcwd(), "reports/reportsInfo.json")):
-        with open("reports/reportsInfo.json", "w") as file:
+    reportsInfoPath = os.path.join(os.getcwd(), "reports", "reportsInfo.json")
+    if not os.path.isfile(reportsInfoPath):
+        with open(reportsInfoPath, "w") as file:
             file.write("{}")
-        #Console logging for debugging purposes
-        print("-----CONSOLE LOGGING-----")
-        print(f"File path that was not found: {os.path.join(str(os.getcwd()), 'reports/reportsInfo.json')}")
-        print("File created and written with empty JSON object")
-        print("-------END OF LOG--------")
 
-
-    with open('reports/reportsInfo.json', 'r') as read_reportsInfo:
+    with open(reportsInfoPath, 'r') as read_reportsInfo:
         loaded_json = json.load(read_reportsInfo)
     
     if report_id not in loaded_json:
-        print("-----CONSOLE LOGGING-----")
-        print(f"Report ID: {report_id} (NOT FOUND)")
-        print(f"Report File Path: NONE")
-        print("Download status: FAILED")
-        print("ERROR: Report ID not found.")
-        print("-------END OF LOG--------")
-        flash("ERROR: The report ID was not found.")
+        flash("ERROR: Invalid report ID.")
         return redirect(url_for('error'))
 
-    report_id_variable = str(report_id)
-
     local_path = os.getcwd() #Need to change this
-    report_file_name = f"report_{report_id_variable}.txt"
+    report_file_name = f"report_{report_id}.txt"
     full_report_file_path = os.path.join(local_path, 'reports', report_file_name)
 
     if not os.path.isfile(full_report_file_path):
-        print("-----CONSOLE LOGGING-----")
-        print(f"Report ID: {report_id} (FOUND)")
-        print(f"Report File Path: {full_report_file_path} (NOT FOUND)")
-        print("Download status: FAILED")
-        print("ERROR: File path not found.")
-        print("-------END OF LOG--------")
-        flash("ERROR: The report file path was not found.")
+        Logger.log("ADMIN REPORT ERROR: Report file was not found for report ID '{}'.".format(report_id))
+        flash("ERROR: Report not found.")
         return redirect(url_for('error'))
     
     Logger.log("ADMIN DOWNLOAD_REPORT: Report with ID '{}' downloaded.".format(report_id))
