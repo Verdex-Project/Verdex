a4ebecc880462f65590b243c9a3b741abf4b7505
Nic <3xpect1916@gmail.com>
2024-01-16 22:46:34 +0800

    Removed my accidental push of a test file

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-from models import *
-sample = datetime.datetime.now().strftime(Universal.systemWideStringDatetimeFormat)
-loaded = datetime.datetime.strptime(sample, Universal.systemWideStringDatetimeFormat)
-toShowUser = datetime.datetime.strftime(loaded, "%d %b %Y %I:%M %p")
-
-print(toShowUser)
