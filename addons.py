19fba9f9dd99c71c47c9d21ad49030d50245c436
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-01-17 10:40:22 +0800

    making temp commit
    halfway through generateRoute method, added googlemaps library

--- addons.py
+++ addons.py
@@ -1,4 +1,4 @@
-import os, sys, json, datetime, copy, pyrebase, uuid
+import os, sys, json, datetime, copy, pyrebase, uuid, googlemaps
 from firebase_admin import db, storage, credentials, initialize_app
 from firebase_admin import auth as adminAuth
 from dotenv import load_dotenv
@@ -570,13 +570,17 @@ class FireAuth:
     
 class GoogleMapsService:
     servicesEnabled = False
+    contextChecked = False
     gmapsAPIKey = None
+    gmapsClient = None
 
     @staticmethod
     def checkContext():
         if "GoogleMapsEnabled" in os.environ and os.environ["GoogleMapsEnabled"] == "True":
+            GoogleMapsService.gmapsClient = googlemaps.Client(key=os.environ["GoogleMapsAPIKey"])
             GoogleMapsService.gmapsAPIKey = os.environ["GoogleMapsAPIKey"]
             GoogleMapsService.servicesEnabled = True
+            GoogleMapsService.contextChecked = True
         
     @staticmethod
     def generateMapEmbedHTML(mapMode, mapType, zoom="default", classlist="", id="", width="600", height="450", placeQuery=None, directionsOrigin=None, directionsDestination=None, directionsMode=None, viewCenterCoordinates=None):
@@ -630,3 +634,37 @@ class GoogleMapsService:
         # Formulate HTML
         embedHTML = "<iframe class='{}' id='{}' width='{}' height='{}' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/{}?{}' allowfullscreen></iframe>".format(classlist, id, width, height, mapMode, "&".join(parameters))
         return embedHTML
+    
+    @staticmethod
+    def generateRoute(startLocation: str, endLocation: str, mode: str, departureTime):
+        # Check permission        
+        if not GoogleMapsService.servicesEnabled:
+            return "ERROR: Google Maps services are not enabled."
+        
+        if (not isinstance(startLocation, str)) or (not isinstance(endLocation, str)) or (not isinstance(mode, str)) or (not isinstance(departureTime, datetime.datetime)):
+            return "ERROR: Invalid start or end location. Both must be a string."
+        elif mode not in ["driving", "walking", "bicycling", "transit", "flying"]:
+            return "ERROR: Invalid directions mode."
+        
+        # Obtain directions data from Google Maps
+        try:
+            directionsData = GoogleMapsService.gmapsClient.directions(startLocation, endLocation, mode=mode, departure_time=departureTime)
+        except Exception as e:
+            return "ERROR: Failed to obtain directions data from Google Maps; error: {}".format(e)
+        
+        # Extract and process relevant data
+        directionsData = directionsData[0]
+        response = {}
+        response["copyright"] = directionsData["copyrights"]
+        response["warnings"] = directionsData["warnings"]
+        response["steps"] = []
+
+        leg = directionsData["legs"][0]
+        ## Process legs
+        for step in leg["steps"]:
+            travelMode = step["travel_mode"]
+            if travelMode == "TRANSIT":
+                # response["steps"][directionsData["steps"].index(step)]["transit_details"] = step["transit_details"]
+                response["steps"][directionsData["steps"].index(step)] = {
+                    "": ""
+                }
