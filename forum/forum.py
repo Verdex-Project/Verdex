495410b630048853820d88d5a93e7505f19a6a8f
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2024-01-19 15:39:05 +0800

    Added check for whether user is banned from forum

--- forum/forum.py
+++ forum/forum.py
@@ -1,12 +1,20 @@
 #Blueprint for VerdexTalks main forum page
 from flask import Blueprint, render_template, json, request, jsonify, redirect, url_for, flash
-from main import Universal, DI
+from main import Universal, DI, manageIDToken
 import datetime
 
 forumBP = Blueprint("forum", __name__)
 
 @forumBP.route('/verdextalks')
 def verdextalks():
+    authCheck = manageIDToken()
+    if not authCheck.startswith("SUCCESS"):
+        return authCheck
+    targetAccountID = authCheck[len("SUCCESS: ")::]
 
+    if DI.data["accounts"][targetAccountID]["forumBanned"] == True:
+        flash("Access Denied. You have been banned from the forum.")
+        return redirect(url_for("error"))
+    
     return render_template("forum/forum.html", postsInfoJson=DI.data["forum"], itineraryInfoJson=DI.data["itineraries"])
     
