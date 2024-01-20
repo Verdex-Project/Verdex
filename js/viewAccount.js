70082adf95e89124e91620a8e3a5b92bef38adcd
JunHam <joonjunhan@gmail.com>
2024-01-20 17:21:16 +0800

    added basic javascript for change password

--- js/viewAccount.js
+++ js/viewAccount.js
@@ -249,3 +249,87 @@ function resendEmail() {
         resendEmailBtn.innerText = "Resend Verification Email"
     })
 }
+
+function changePassword() {
+    var currentPassword = document.getElementById("currentPasswordInput");
+    var newPassword = document.getElementById("newPasswordInput");
+    var cfmPassword = document.getElementById("cfmPasswordInput");
+    const changePasswordMsg = document.getElementById("changePasswordMsg")
+    const saveBtn = document.getElementById("modalSaveBtn");
+    const modal = new bootstrap.Modal(document.getElementById("newPasswordModal"));
+
+    changePasswordMsg.style.visibility = 'visible'
+
+    if (!currentPassword.value || currentPassword.value == "" || !newPassword.value || newPassword.value == "" || !cfmPassword.value || cfmPassword.value == "") {
+        changePasswordMsg.innerHTML = "Please fill in all the fields."
+        return
+    }
+
+    if (currentPassword == newPassword) {
+        changePasswordMsg.innerHTML = "New password must differ from the current password."
+        return
+    }
+
+    if (newPassword.value !== cfmPassword.value) {
+        changePasswordMsg.innerHTML = "Passwords do not match."
+        return
+    }
+    
+    saveBtn.disabled = true
+    saveBtn.innerText = "Saving Changes..."
+
+    location.reload()
+    console.log("Pass")
+
+    // axios({
+    //     method: 'post',
+    //     url: '/api/createAccount',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'VerdexAPIKey': '\{{ API_KEY }}'
+    //     },
+    //     data: {
+    //         "currentPassword": currentPassword.value,
+    //         "newPassword": newPassword.value,
+    //         "cfmNewPassword": cfmPassword.value
+    //     }
+    // })
+    // .then(response => {
+    //     if (response.status == 200) {
+    //         if (!response.data.startsWith("ERROR:")) {
+    //             if (!response.data.startsWith("UERROR:")) {
+    //                 if (response.data.startsWith("SUCCESS:")) {
+    //                     // signUpButton.innerHTML = "Account created! Redirecting now..."
+    //                     // location.href = `${origin}/account/info`;
+    //                 } else {
+    //                     // cfmPasswordMsg.style.color = 'red'
+    //                     // cfmPasswordMsg.innerText = "An unknown error occured in creating the account. Please try again."
+    //                     console.log("Unknown response received: " + response.data)
+    //                 }
+    //             } else {
+    //                 console.log("User error occured: " + response.data)
+    //                 cfmPasswordMsg.style.color = 'red'
+    //                 cfmPasswordMsg.innerText = response.data.substring("UERROR: ".length)
+    //             }
+    //         } else {
+    //             cfmPasswordMsg.style.color = 'red'
+    //             cfmPasswordMsg.innerText = "An error occured in creating your account. Please try again."
+    //             console.log("Error occured in making login request: " + response.data)
+    //         }
+    //     } else {
+    //         cfmPasswordMsg.style.color = 'red'
+    //         cfmPasswordMsg.innerText = "An error occured while connecting to Verdex Servers. Please try again later."
+    //         console.log("Non-200 responnse status code recieved from Verdex Servers.")
+    //     }
+    //     signUpButton.disabled = false
+    //     signUpButton.innerText = "Sign Up"
+    // })
+    // .catch(err => {
+    //     console.log("An error occured in connecting to Verdex Servers: " + err)
+    //     cfmPasswordMsg.style.color = 'red'
+    //     cfmPasswordMsg.innerText = "An error occured in connecting to Verdex Servers. Please try again later."
+    //     signUpButton.disabled = false
+    //     signUpButton.innerText = "Sign Up"
+    // })
+
+}
