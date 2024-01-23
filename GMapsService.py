6b592b9e5f373d770c1067a3390df54f37864f36
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-01-23 10:56:14 +0800

    added documentation for map embed method

--- GMapsService.py
+++ GMapsService.py
@@ -19,6 +19,58 @@ class GoogleMapsService:
         
     @staticmethod
     def generateMapEmbedHTML(mapMode, mapType, zoom="default", classlist="", id="", width="600", height="450", placeQuery=None, directionsOrigin=None, directionsDestination=None, directionsMode=None, viewCenterCoordinates=None):
+        '''Returns a string of `iframe` HTML that has a fully-embedded Google Map.
+
+        ## Parameters:
+        - `mapMode`: String --> `place`, `directions`, `view`
+            - `place` to show the map of a specific place with a marker on the place's location
+                - Requires `placeQuery`.
+            - `directions` shows a map with a route plotted between a start and end destination.
+                - Requires `directionsOrigin`, `directionsDestination`, and `directionsMode`
+            - `view` displays a map around specific center co-ordinates. 
+                - Requires `viewCenterCoordinates`.
+        - `mapType`: String --> `roadmap`, `satellite`
+        - `zoom`: String (optional, default: automatic) -->  Integer in a string. Ranges from 0 (the whole world) to 21 (individual buildings).
+        - `classlist`: String (optional) --> Will set the classlist you provide in the `iframe`'s HTML. Could be used to apply CSS rules to the `iframe` embed.
+        - `id`: String (optional) --> Will set the ID you provide in the `iframe`'s HTML. Could be used to apply CSS rules to the `iframe` embed or otherwise.
+        - `width`: String (optional, default: 600) --> Sets the width of the `iframe`. All normal HTML and CSS width sizes can be applied (e.g `10%`).
+        - `height`: String (optional, default: 450) --> Sets the height of the `iframe`. All normal HTML and CSS width sizes can be applied (e.g `10%`).
+        - `placeQuery`: String (optional) --> Query a specific place on Google Maps. Example value: `Marina Bay Sands`.
+        - `directionsOrigin`: String (optional) --> A query for the origin of a directions route plot. Example value: `Marina Bay Sands`.
+        - `directionsDestination`: String (optional) --> A query for the destination of a directions route plot. Example value: `Jewel Changi Airport`.
+        - `directionsMode`: String (optional) --> The mode of transit for a directions route plot. Valid values: `driving`, `walking`, `bicycling`, `transit`, `flying`
+        - `viewCenterCoordinates`: String (optional) --> Comma-separated latitude and longitude coordinates. Example value: `37.4218,-122.0840`.
+
+        ## Sample usage
+        In a Python file:
+        ```python
+        GoogleMapsService.checkContext()
+        @app.route("/map")
+        def showMap():
+            mapEmbed = GoogleMapsService.generateMapEmbedHTML(mapMode="place", mapType="satellite", classlist="maps", id="googleMap", placeQuery="Marina Bay Sands")
+            return render_template("Map.html", map=mapEmbed)
+        ```
+
+        In an HTML template `Map.html`:
+        ```html
+        <html>
+            <head>
+                <style>
+                    .maps {
+                        text-align: center;
+                    }
+                </style>
+            </head>
+            <body>
+                {{ map|safe }}
+            </body>
+        </html>
+        ```
+
+        NOTE: To use `GoogleMapsService`, a Google Maps Platform API Key must be set as `GoogleMapsAPIKey` and `GoogleMapsEnabled` must be set to `True` in the .env file.
+        '''
+
+        
         # Check permission        
         if not GoogleMapsService.servicesEnabled:
             return "ERROR: Google Maps services are not enabled."
