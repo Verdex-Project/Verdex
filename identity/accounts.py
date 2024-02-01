602989113a28ab491e3b87996c46417494094c68
JunHam <joonjunhan@gmail.com>
2024-02-01 10:41:52 +0800

    added upload_file in accounts.py

--- identity/accounts.py
+++ identity/accounts.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, Blueprint, session, redirect, url_for, request, flash
 from flask_cors import CORS
-from main import DI, FireAuth, Universal, manageIDToken, Logger, secure_filename, allowed_file, app
+from main import DI, FireAuth, Universal, manageIDToken, Logger, secure_filename, allowed_file, app, FolderManager
 
 accountsBP = Blueprint("accounts",__name__)
 
@@ -50,18 +50,54 @@ def myAccount():
 
 @accountsBP.route('/', methods=['GET', 'POST'])
 def upload_file():
+    authCheck = manageIDToken()
+    if not authCheck.startswith("SUCCESS"):
+        return redirect(url_for("unauthorised", error=authCheck[len("ERROR: ")::]))
+    targetAccountID = authCheck[len("SUCCESS: ")::]
+
+    #Register folder via FolderManager if not registered
+    if not FolderManager.checkIfFolderIsRegistered(filename):
+                response = FolderManager.registerFolder(filename)
+                if response.startswith("ERROR"):
+                    Logger.log("ACCOUNTS UPLOAD_FILE ERROR: Failed to register file for account id {}; response: {}".format(targetAccountID, response))
+                    flash("Failed to register a folder in the system for your account. Please try again.")
+                    return redirect(request.url)
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+        
         file = request.files['file']
+
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            # Check account folder for a previous pfp, if exists, delete
+            if (not isinstance(FolderManager.getFilenames(targetAccountID))) and (not FolderManager.getFilenames().startswith("ERROR")):
+                try:
+                    response = FolderManager.deleteFile(targetAccountID, FolderManager.getFilenames(targetAccountID))
+                    if response.startswith("ERROR"):
+                        Logger.log("ACCOUNTS UPLOAD_FILE ERROR: Failed to delete existing folder for account id {}".format(targetAccountID))
+                        flash("An error occured in uploading your file. Please try again.")
+                        return redirect(request.url)
+                except Exception as e:
+                    Logger.log("ACCOUNTS UPLOAD_FILE ERROR: Failed to delete existing folder for account id {}".format(targetAccountID))
+                    flash("An error occured in uploading your file. Please try again.")
+                    return redirect(request.url)
+
+            fileExtension = '.' in filename and filename.rsplit('.', 1)[1].lower()
+
+            filename = secure_filename(targetAccountID, "pfp.", fileExtension)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('download_file', name=filename))
+            return redirect(request.url)
+        else:
+            flash("File extension not allowed.")
+            return redirect(request.url)
+    else:
+        return render_template('')
