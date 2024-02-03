1689cb421b698d4c7a5c6407c642d2f0caf57d39
JunHam <joonjunhan@gmail.com>
2024-02-04 02:00:26 +0800

    added listing of my itineraries in view account page

--- identity/accounts.py
+++ identity/accounts.py
@@ -79,10 +79,17 @@ def myAccount():
         DI.data["accounts"][targetAccountID]["aboutMe"] = "Tell us more about yourself!"
         DI.save()
 
+    # itineraryID = None
+    # for itinerary_id, itinerary_info in DI.data["itineraries"].items():
+    #     if "associatedAccountID" in itinerary_info and itinerary_info["associatedAccountID"] == targetAccountID:
+    #         itineraryID = itinerary_id
+    #         break
+
     targetAccount = DI.data["accounts"][targetAccountID]
     username = targetAccount["username"]
     email = targetAccount["email"]
     aboutMeDescription = targetAccount["aboutMe"]
+    itineraries = DI.data["itineraries"]
 
     ## Check email verification
     notVerified = False
@@ -93,4 +100,4 @@ def myAccount():
     else:
         notVerified = not accInfo["emailVerified"]
 
-    return render_template("identity/viewAccount.html", username=username, email=email, aboutMeDescription=aboutMeDescription, emailNotVerified=notVerified)
+    return render_template("identity/viewAccount.html", username=username, email=email, aboutMeDescription=aboutMeDescription, emailNotVerified=notVerified, accID = targetAccountID, itineraries = itineraries)
