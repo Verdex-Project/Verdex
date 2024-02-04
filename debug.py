950dbeb16d9373c47a531c8a088d442ba9d86397
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-02-04 21:59:03 +0800

    made a quick to use debug file

--- debug.py
+++ debug.py
@@ -1,12 +1,24 @@
 from models import *
+import shutil
 
 FireAuth.connect()
 FireConn.connect()
 
-input("Do you want to reset all Firebase data? (Press Enter to continue) ")
+choice = input("Do you want to reset all Firebase data or wipe all local persistent files? (1/2) ")
 print("Resetting...")
 
-for fireUser in FireAuth.listUsers():
-    FireAuth.deleteAccount(fireUser.uid, admin=True)
+if choice == "1":
+    for fireUser in FireAuth.listUsers():
+        FireAuth.deleteAccount(fireUser.uid, admin=True)
 
-FireRTDB.setRef({})
+    FireRTDB.setRef({})
+elif choice == "2":
+    for file in ["config.txt", "database.txt", "analytics.json"]:
+        if os.path.isfile(file):
+            os.remove(file)
+        
+    for folder in ["UserFolders", "reports"]:
+        if os.path.isdir(folder):
+            shutil.rmtree(folder)
+
+print("Done.")
