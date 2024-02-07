937abb3ed41eb170c6805d164a3f59a5fdd486f1
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-02-07 18:10:02 +0800

    conditional rendering of gpt button

--- generation/itineraryGeneration.py
+++ generation/itineraryGeneration.py
@@ -1,9 +1,10 @@
 from flask import Flask, Blueprint, render_template, url_for, request, redirect, flash, send_file, send_from_directory
-from main import DI, manageIDToken, Logger, Universal
+from main import DI, manageIDToken, Logger, Universal, AddonsManager
 import os, json, datetime, random
 
 itineraryGenBP = Blueprint('itineraryGen', __name__)
 
 @itineraryGenBP.route("/generate/targetLocations")
 def targetLocations():
-    return render_template("generation/targetLocations.html", popularLocations=Universal.generationData["locations"])
+    verdexGPTEnabled = AddonsManager.readConfigKey("VerdexGPTEnabled") == True
+    return render_template("generation/targetLocations.html", popularLocations=Universal.generationData["locations"], gptEnabled=verdexGPTEnabled)
